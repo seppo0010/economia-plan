d958e68630bf3dcd30109cd64dd84380fb450fca
Sebastian Waisbrot <seppo0010@gmail.com>
2021-05-27 10:31:21 -0300

    Refactoring

--- src/DependencyChart.tsx
+++ src/DependencyChart.tsx
@@ -4,28 +4,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from './store'
 import { toggleSubject } from './subjectsStatus'
 import { getGraphData } from './plan'
+import { subjectsRect, canDo } from './dependencyChartData'
 
-const subjectsRect = {
-  131: [268, 201, 136, 62],
-  144: [266, 358, 136, 62],
-  132: [268, 551, 136, 62],
-  135: [268, 657, 136, 62],
-  138: [479, 775, 133, 59],
-  136: [479, 657, 133, 59],
-  134: [479, 552, 133, 59],
-  142: [718, 774, 133, 59],
-  137: [718, 658, 133, 59],
-  133: [716, 551, 136, 62],
-  141: [718, 316, 133, 59],
-  139: [718, 211, 133, 59],
-  162: [954, 258, 134, 60],
-  145: [954, 434, 133, 59],
-  140: [954, 552, 133, 59],
-  167: [954, 657, 134, 60],
-  163: [952, 831, 134, 60],
-  169: [1187, 552, 134, 60],
-  168: [1187, 255, 134, 60],
-}
 function DependencyChart() {
   const checked = useSelector((state: RootState) => state.subjectsStatus);
   const dispatch = useDispatch()
@@ -38,20 +18,19 @@ function DependencyChart() {
       setGraphData(await getGraphData())
     })()
   })
-  const canDo = (subject: string) => {
-      if (checked.has(subject) || !graphData) {
+  const _canDo = (subject: string) => {
+      if (!graphData) {
         return false;
       }
-      const defaultEdges: {edges: {from: string, to: string}[]} = {edges: []}
-      const requirements = (graphData || defaultEdges).edges.filter((e) => e.to.toString() === subject).map((e) => e.from.toString())
-      return requirements.every((requirement) => checked.has(requirement))
+      const defaultEdges = {edges: []}
+      return canDo(subject, checked, graphData || defaultEdges)
   }
   return (
       <div>
           <svg width="1406" height="946" viewBox="0 0 1406 946"  xmlns="http://www.w3.org/2000/svg">
               <image href="/acercade_correlatividades.jpg" width="1406" height="946"/>
               {Object.entries(subjectsRect).map(([key, value]) => (
-                <rect key={key} x={value[0]} y={value[1]} width={value[2]} height={value[3]} fill={checked.has(key) ? 'red' : canDo(key) ? 'yellow' : 'transparent'} fillOpacity="0.7" rx="22" ry="22" onClick={() => handleClick(key.toString())} cursor="pointer" />
+                <rect key={key} x={value[0]} y={value[1]} width={value[2]} height={value[3]} fill={checked.has(key) ? 'red' : _canDo(key) ? 'yellow' : 'transparent'} fillOpacity="0.7" rx="22" ry="22" onClick={() => handleClick(key.toString())} cursor="pointer" />
               ))}
           </svg>
       </div>
---
+++ src/dependencyChartData.tsx
@@ -0,0 +1,29 @@
+const subjectsRect = {
+  131: [268, 201, 136, 62],
+  144: [266, 358, 136, 62],
+  132: [268, 551, 136, 62],
+  135: [268, 657, 136, 62],
+  138: [479, 775, 133, 59],
+  136: [479, 657, 133, 59],
+  134: [479, 552, 133, 59],
+  142: [718, 774, 133, 59],
+  137: [718, 658, 133, 59],
+  133: [716, 551, 136, 62],
+  141: [718, 316, 133, 59],
+  139: [718, 211, 133, 59],
+  162: [954, 258, 134, 60],
+  145: [954, 434, 133, 59],
+  140: [954, 552, 133, 59],
+  167: [954, 657, 134, 60],
+  163: [952, 831, 134, 60],
+  169: [1187, 552, 134, 60],
+  168: [1187, 255, 134, 60],
+};
+const canDo = (subject: string, checked: Set<string>, graphData: {edges: {from: string, to: string}[]}) => {
+  if (checked.has(subject)) {
+    return false;
+  }
+  const requirements = graphData.edges.filter((e) => e.to.toString() === subject).map((e) => e.from.toString())
+  return requirements.every((requirement) => checked.has(requirement))
+}
+export { subjectsRect, canDo };
