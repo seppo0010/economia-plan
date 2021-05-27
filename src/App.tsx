3a64a87a7a24961d5a24f232b30b19948aecbf6f
Sebastian Waisbrot <seppo0010@gmail.com>
2021-05-27 08:18:30 -0300

    Add dependency chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import DependencyChart from './DependencyChart'
 
 function App() {
   return (
     <div className="App">
+        <DependencyChart />
     </div>
   );
 }
---
+++ src/DependencyChart.tsx
@@ -0,0 +1,32 @@
+import './DependencyChart.css'
+
+function DependencyChart() {
+    return (
+        <div>
+            <svg width="1406" height="946" viewBox="0 0 1406 946"  xmlns="http://www.w3.org/2000/svg">
+                <image href="/acercade_correlatividades.jpg" width="1406" height="946"/>
+                <rect x="268" y="201" width="136" height="62" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia131" />
+                <rect x="266" y="358" width="136" height="62" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia144" />
+                <rect x="268" y="551" width="136" height="62" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia132" />
+                <rect x="268" y="657" width="136" height="62" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia135" />
+                <rect x="479" y="775" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia138" />
+                <rect x="479" y="657" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia136" />
+                <rect x="479" y="552" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia134" />
+                <rect x="718" y="774" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia142" />
+                <rect x="718" y="658" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia137" />
+                <rect x="716" y="551" width="136" height="62" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia133" />
+                <rect x="718" y="316" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia141" />
+                <rect x="718" y="211" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia139" />
+                <rect x="954" y="258" width="134" height="60" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia162" />
+                <rect x="954" y="434" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia145" />
+                <rect x="954" y="552" width="133" height="59" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia140" />
+                <rect x="954" y="657" width="134" height="60" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia167" />
+                <rect x="952" y="831" width="134" height="60" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia163" />
+                <rect x="1187" y="552" width="134" height="60" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia169" />
+                <rect x="1187" y="255" width="134" height="60" fill="red" fillOpacity="0.7" rx="22" ry="22" id="materia168" />
+            </svg>
+        </div>
+    )
+}
+
+export default DependencyChart
