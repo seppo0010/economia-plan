7d198fd0eb4153e10d75a3a3abf8be962013144b
Sebastian Waisbrot <seppo0010@gmail.com>
2021-05-29 22:25:06 -0300

    Fix plan image path

--- src/DependencyChart.tsx
+++ src/DependencyChart.tsx
@@ -5,6 +5,7 @@ import { RootState } from './store'
 import { toggleSubject } from './subjectsStatus'
 import { getGraphData, GraphData } from './plan'
 import { subjectsRect, canDo } from './dependencyChartData'
+import acercade_correlatividades from './acercade_correlatividades.jpg'
 
 function DependencyChart() {
   const checked = useSelector((state: RootState) => state.subjectsStatus);
@@ -28,7 +29,7 @@ function DependencyChart() {
   return (
       <div>
           <svg viewBox="0 0 1406 946" xmlns="http://www.w3.org/2000/svg">
-              <image href="/acercade_correlatividades.jpg" width="1406" height="946"/>
+              <image href={acercade_correlatividades} width="1406" height="946"/>
               {Object.entries(subjectsRect).map(([key, value]) => (
                 <rect key={key} x={value[0]} y={value[1]} width={value[2]} height={value[3]} fill={checked.has(key) ? 'red' : _canDo(key) ? 'yellow' : 'transparent'} fillOpacity="0.7" rx="22" ry="22" onClick={() => handleClick(key.toString())} cursor="pointer" />
               ))}
